00ae9622c6b7311bea3013e308abee429790a62e
jstation <sophiabeachanel@gmail.com>
2026-08-22 20:31:59 +0800

    s

--- core/src/assemble.d
+++ core/src/assemble.d
@@ -4,10 +4,10 @@ import std.path;
 
 // entry point for compilation
 int main(String[] args) {
-    if(!exists(src) || !isDir(src)) {
+    if(!exists(args[0]) || !isDir(args[0])) {
         return(2); // the file could not be found or is not a directory
     }
 
     string src = args[0];
-    string out = args[1] == null || !exists(src) || !isDir(src) ? dirName(src): args[1];
+    string out = args[1] == null || !exists(src) || !isDir(src) ? dirName(src) : args[1];
 }
